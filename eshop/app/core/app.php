91839032550ccd6199b75bbcaa5871b4c2b1a783
Ricardo Cabrita <ricardo.cabrita1692@gmail.com>
2024-01-12 13:25:34 +0000

    UPDATE

--- eshop/app/core/app.php
+++ eshop/app/core/app.php
@@ -18,9 +18,16 @@ Class App
         $this->controller = strtolower($url[0]);
         unset($url[0]);
         
+
     }
         require "../app/controllers/" . $this->controller . ".php";
         $this->controller = new $this->controller;
+        if(isset($url[1]))
+        {
+            $url[1] = strtolower($url[1]);
+            if(method_exists($this->controller,$url[1]));
+
+        }
         show ($url);
 
     }
