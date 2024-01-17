6ca2b53dc3e1fabe9d7e95c63c3fc837a6f3c350
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2024-01-17 11:35:56 +0000

    housekeeping

--- eshop/app/init.php
+++ eshop/app/init.php
@@ -1,6 +1,6 @@
 <?php
 
 include "../app/core/app.php";
-include "../app/core/controler.php";
+include "../app/core/controller.php";
 include "../app/core/database.php";
 include "../app/core/functions.php";
