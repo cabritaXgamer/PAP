3f93c85c12d200fb392fadb200b066b691f429f2
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2024-01-17 10:29:17 +0000

    ADD routing system to controllers in app file core

---
+++ eshop/app/controllers/products.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class Products
+{
+    //Public default metodo index, mesmo que o utilizador coloque ou não qualquer URL, o Index vai sempre correr
+    public function index()
+     {
+
+        echo "This is the ==> PRODUCTS CLASS <=== class inside the index method.";
+
+     }
+}
--- eshop/app/core/app.php
+++ eshop/app/core/app.php
@@ -9,12 +9,45 @@ class App
 {
     protected $controller = "home"; 
     protected $method ="index";
+    protected $params;
 
     //Constructor
     public function __construct()
     {
         $url= $this->parseURL();
-        show($url);
+        //show($url);
+
+        //Se o ficheiro existir, ele vai substituir o controlador para aceder às funçoes do controlador especifico
+        //Concatena a URL com as funçoes 
+        if(file_exists("../app/controllers/" . strtolower($url[0]) . ".php"))
+        {
+            $this->controller = strtolower($url[0]);
+            unset($url[0]);
+        }
+
+        //Senao encontrar o controlador ele vai para a home
+        require "../app/controllers/" . $this->controller . ".php";
+        $this->controller = new $this->controller;
+        
+        //Aqui vamos procurar um metodo dentro do controldaor no array pos [1] da URL
+        if(isset($url[1]))
+        {
+            //primeiro verificamos se existe um um metodo pos[2] da URL
+            $url[1] = strtolower($url[1]);
+            if(method_exists($this->controller, $url[1]))
+            {
+                //remover 
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        //Validação se nao existir nada na url ele manda para a home, caso contrario
+        //O params pode receber o array fornecido pelo utilizador, caso contrario recebe a home 
+        $this->params = (count($url) > 0) ? $url : ["home"];
+        //show(array_values($url));
+
+        call_user_func_array([$this->controller,$this->method],$this->params);
     }
 
     //Serve para 
