f9a9225023e1d480785861d87ee04f16e6bee8e9
Ricardo Cabrita <ricardo.cabrita1692@gmail.com>
2024-01-23 22:10:37 +0000

    UPDATE file index

--- eshop/public/index.php
+++ eshop/public/index.php
@@ -4,8 +4,9 @@ session_start();
 
 
 
-//$a = $_SERVER['REQUEST_SCREME'] . "://". $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'];
+$a = $_SERVER['REQUEST_SCREME'] . "://". $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'] .$_SERVER['PHP_SELF'];
+$a = str_replace("index.php", "", $a);
 include "../app/init.php";
-
+show($a);
 $app = new App();
 
