44a0946b768b591d0d71138480fe00e3a3b542b2
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2024-05-13 22:23:35 +0100

    house keeping

--- papshop/app/views/admin/_includes/admin_footer.php
+++ papshop/app/views/admin/_includes/admin_footer.php
@@ -2,7 +2,7 @@
    <footer class="site-footer">
           <div class="text-center">
               2014 - Alvarez.is
-              <a href="index.html#" class="go-top">
+              <a href="index" class="go-top">
                   <i class="fa fa-angle-up"></i>
               </a>
           </div>
@@ -11,24 +11,24 @@
   </section>
 
     <!-- js placed at the end of the document so the pages load faster -->
-    <script src="assets/js/jquery.js"></script>
-    <script src="assets/js/jquery-1.8.3.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
-    <script src="assets/js/jquery.scrollTo.min.js"></script>
-    <script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
-    <script src="assets/js/jquery.sparkline.js"></script>
+    <script src="<?= ASSETS . ADMIN_THEME ?>/js/jquery.js"></script>
+    <script src="<?= ASSETS . ADMIN_THEME ?>/js/jquery-1.8.3.min.js"></script>
+    <script src="<?= ASSETS . ADMIN_THEME ?>/js/bootstrap.min.js"></script>
+    <script class="include" type="text/javascript" src="<?= ASSETS . ADMIN_THEME ?>/js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="<?= ASSETS . ADMIN_THEME ?>/js/jquery.scrollTo.min.js"></script>
+    <script src="<?= ASSETS . ADMIN_THEME ?>/js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script src="<?= ASSETS . ADMIN_THEME ?>/js/jquery.sparkline.js"></script>
 
 
     <!--common script for all pages-->
-    <script src="assets/js/common-scripts.js"></script>
+    <script src="<?= ASSETS . ADMIN_THEME ?>/js/common-scripts.js"></script>
     
-    <script type="text/javascript" src="assets/js/gritter/js/jquery.gritter.js"></script>
-    <script type="text/javascript" src="assets/js/gritter-conf.js"></script>
+    <script type="text/javascript" src="<?= ASSETS . ADMIN_THEME ?>/js/gritter/js/jquery.gritter.js"></script>
+    <script type="text/javascript" src="<?= ASSETS . ADMIN_THEME ?>/js/gritter-conf.js"></script>
 
     <!--script for this page-->
-    <script src="assets/js/sparkline-chart.js"></script>    
-	<script src="assets/js/zabuto_calendar.js"></script>	
+    <script src="<?= ASSETS . ADMIN_THEME ?>/js/sparkline-chart.js"></script>    
+	<script src="<?= ASSETS . ADMIN_THEME ?>/js/zabuto_calendar.js"></script>	
 	
 	<script type="text/javascript">
         $(document).ready(function () {
--- papshop/app/views/admin/_includes/admin_menu.php
+++ papshop/app/views/admin/_includes/admin_menu.php
@@ -7,78 +7,73 @@
               <!-- sidebar menu start-->
               <ul class="sidebar-menu" id="nav-accordion">
               
-              	  <p class="centered"><a href="profile.html"><img src="assets/img/ui-sam.jpg" class="img-circle" width="60"></a></p>
-              	  <h5 class="centered">Marcel Newman</h5>
+              	  <p class="centered"><a href="profile.html"><img src="<?= ASSETS . ADMIN_THEME ?>/img/ui-sam.jpg" class="img-circle" width="60"></a></p>
+              	  <h5 class="centered"><?php echo $data['user_data']->name ?></h5>
               	  	
                   <li class="mt">
-                      <a class="active" href="index.html">
+                      <a class="active" href="<?= ROOT ?>admin/index">
                           <i class="fa fa-dashboard"></i>
                           <span>Dashboard</span>
                       </a>
                   </li>
 
                   <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class="fa fa-desktop"></i>
-                          <span>UI Elements</span>
+                      <a href="<?= ROOT ?>admin/products" >
+                          <i class="fa fa-barcode"></i>
+                          <span>Products</span>
                       </a>
                       <ul class="sub">
-                          <li><a  href="general.html">General</a></li>
-                          <li><a  href="buttons.html">Buttons</a></li>
-                          <li><a  href="panels.html">Panels</a></li>
+                          <li><a  href="<?= ROOT ?>admin/products/add">Add New Product</a></li>
+                          <li><a  href="<?= ROOT ?>admin/products/edit">Edit Product</a></li>
+                          <li><a  href="<?= ROOT ?>admin/products/delete">Delete Product</a></li>
                       </ul>
                   </li>
 
                   <li class="sub-menu">
-                      <a href="javascript:;" >
+                      <a href="<?= ROOT ?>admin/categories" >
+                          <i class="fa fa-list-alt"></i>
+                          <span>Categories</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="<?= ROOT ?>admin/categories/add">Add New Categories</a></li>
+                          <li><a  href="<?= ROOT ?>admin/categories/edit">Edit Categories</a></li>
+                          <li><a  href="<?= ROOT ?>admin/categories/delete">Delete Categories</a></li>
+                      </ul>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="<?= ROOT ?>admin/orders" >
+                          <i class="fa fa-reorder"></i>
+                          <span>Orders</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="<?= ROOT ?>admin/settings" >
                           <i class="fa fa-cogs"></i>
-                          <span>Components</span>
+                          <span>Settings</span>
                       </a>
                       <ul class="sub">
-                          <li><a  href="calendar.html">Calendar</a></li>
-                          <li><a  href="gallery.html">Gallery</a></li>
-                          <li><a  href="todo_list.html">Todo List</a></li>
+                          <li><a  href="<?= ROOT ?>admin/settings/slider">Shop Images</a></li>
                       </ul>
                   </li>
+
                   <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class="fa fa-book"></i>
-                          <span>Extra Pages</span>
+                      <a href="<?= ROOT ?>admin/users" >
+                          <i class="fa fa-user"></i>
+                          <span>Users</span>
                       </a>
                       <ul class="sub">
-                          <li><a  href="blank.html">Blank Page</a></li>
-                          <li><a  href="login.html">Login</a></li>
-                          <li><a  href="lock_screen.html">Lock Screen</a></li>
+                          <li><a  href="<?= ROOT ?>admin/users/costumers">Costumers</a></li>
+                          <li><a  href="<?= ROOT ?>admin/users/admins">Admins</a></li>
                       </ul>
                   </li>
+
                   <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class="fa fa-tasks"></i>
-                          <span>Forms</span>
+                      <a href="<?= ROOT ?>admin/backup" >
+                          <i class="fa fa-hdd-o"></i>
+                          <span>WebSite Backup</span>
                       </a>
-                      <ul class="sub">
-                          <li><a  href="form_component.html">Form Components</a></li>
-                      </ul>
-                  </li>
-                  <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class="fa fa-th"></i>
-                          <span>Data Tables</span>
-                      </a>
-                      <ul class="sub">
-                          <li><a  href="basic_table.html">Basic Table</a></li>
-                          <li><a  href="responsive_table.html">Responsive Table</a></li>
-                      </ul>
-                  </li>
-                  <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class=" fa fa-bar-chart-o"></i>
-                          <span>Charts</span>
-                      </a>
-                      <ul class="sub">
-                          <li><a  href="morris.html">Morris</a></li>
-                          <li><a  href="chartjs.html">Chartjs</a></li>
-                      </ul>
                   </li>
 
               </ul>
